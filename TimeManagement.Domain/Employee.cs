ff1365dab8b801c6bb62127bdfd5b5a612706a1c
Vincent Beilke <vincent.beilke@gmail.com>
2022-06-10 14:53:38 -0500

    Added Employee and Manager classes

---
+++ TimeManagement.Domain/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManagement.Domain
+{
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Gender { get; set; }
+
+        public virtual string FullName()
+        {
+            return $"{LastName}, {FirstName}";
+        }
+    }
+}
---
+++ TimeManagement.Domain/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManagement.Domain
+{
+    public class Manager : Employee
+    {
+        public List<Employee> Employees { get; set; }
+
+        public override string FullName()
+        {
+            return $"Manager : {base.FullName()}";
+        }
+    }
+}
